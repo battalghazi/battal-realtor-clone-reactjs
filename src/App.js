e5d8da223c95589892c82f81fe72409f5d55d11f
SAİT ÜMİT ÜNAL <SUmit.Unal@afetacil.gov.tr>
2023-06-03 15:18:19 +0300

    Created pages and routes iyi günler cevriye henimcimm altdudak

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import ForgetPassword from "./pages/ForgetPassword";
+import Offers from "./pages/Offers";
 
 
 function App() {
   return (
     <>
-      <h1 className="text-2xl bg-red-400">Bu sefer React</h1>
+     <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/forgetpassword" element={<ForgetPassword />} />
+        <Route path="/offers" element={<Offers />} />
+      </Routes>
+     </Router>
     </>
   );
 }
